0791adcf306fe41c76aa433c6ff2bf28028f9ace
Michael Große <mic.grosse@googlemail.com>
2021-03-23 16:25:36 +0100

    refactor: mock with MirageJS instead of commenting code
    This will cause less errors and allows using npm run serve, i.e., the
    vuejs dev server. That will development significantly faster.

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import createServices from '@/services/createServices';
 import createStore from './store';
+import { makeServer } from "@/mocks/mirageServer"
+
+if ( process.env.NODE_ENV === "development" ) {
+	makeServer()
+}
 
 const services = createServices();
 
