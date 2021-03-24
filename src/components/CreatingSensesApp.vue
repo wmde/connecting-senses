3a6b5e49c306358d3e1d7cfc0c32e5fc22431967
Michael Große <michael.grosse@wikimedia.de>
2021-03-24 19:38:51 +0100

    feat: query and show senses (#17)
    Hard-coding English and ltr as the overall html language and direction
    and deviating from it for the sense and lemma.

--- src/components/CreatingSensesApp.vue
+++ src/components/CreatingSensesApp.vue
@@ -8,6 +8,7 @@
 			<Intro />
 			<Login v-if="userDisplayName === null" />
 			<LanguagePicker v-else />
+			<Sense v-if="sense" :sense-info="sense" :language-code="languageCode" />
 		</main>
 	</div>
 </template>
@@ -18,12 +19,20 @@ import Login from '@/components/Login.vue';
 import UserTools from '@/components/UserTools.vue';
 import Intro from '@/components/Intro.vue';
 import LanguagePicker from '@/components/LanguagePicker.vue';
+import { SenseInfo } from '@/data-access/SensesRepository';
+import Sense from '@/components/Sense.vue';
 
 export default defineComponent( {
-	components: { Intro, LanguagePicker, UserTools, Login },
+	components: { Sense, Intro, LanguagePicker, UserTools, Login },
 	computed: {
 		userDisplayName(): null | string {
 			return this.$store.getters.userDisplayName;
+		},
+		sense(): SenseInfo | null {
+			return this.$store.getters.sense;
+		},
+		languageCode(): string | null {
+			return this.$store.getters.languageCode;
 		},
 	},
 } );
--- src/store/getters.ts
+++ src/store/getters.ts
@@ -1,4 +1,5 @@
 import { RootState } from '@/store/index';
+import { SenseInfo } from '@/data-access/SensesRepository';
 
 export default {
 	userDisplayName( rootState: RootState ): null | string {
@@ -10,5 +11,14 @@ export default {
 	},
 	languageLabel( rootState: RootState ): string | null {
 		return rootState.language?.label ?? null;
+	},
+	languageCode( rootState: RootState ): string | null {
+		return rootState.language?.code ?? null;
+	},
+	sense( rootState: RootState ): SenseInfo | null {
+		if ( rootState.senses.length === 0 ) {
+			return null;
+		}
+		return rootState.senses[ 0 ];
 	},
 };
