913616cd5e6be7913a7afedf553761f34ac43757
Itamar Givon <itamar.givon@wikimedia.de>
2021-03-24 18:37:31 +0100

    refactor: improve logging

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require( "express" );
 const session = require( "express-session" );
 const bodyParser = require( "body-parser" );
 const createError = require( "http-errors" );
+const logger = require('morgan');
 
 const passport = require( "./server/passport-setup" );
 const router = require( "./server/routes" );
@@ -15,6 +16,10 @@ app.set( "views", __dirname + "/dist" );
 app.use( express.static(__dirname + "/dist") );
 
 app.use( bodyParser.json() );
+app.use( logger( "dev", {
+	skip: function (req, res) { return res.statusCode < 400 }
+} ) );
+app.use( express.json() );
 
 app.use( passport.initialize() );
 app.use( passport.session() );
@@ -34,9 +39,9 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
 	res.status(err.status || 500);
-	if( !err.expose ) {
-		console.error(err);
 
+	if( !err.expose ) {
+		console.error(`[${new Date().toISOString()}]` + err.message);
 		return res.json({ 
 			message: 'Internal server error.'  
 		});
