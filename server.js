58b42ed6519b9e791dbe1a4e5300b3604d179d9e
Itamar Givon <itamar.givon.dev@gmail.com>
2021-03-24 19:55:41 +0100

    feature: decision tracking (#14)
    * fix: alias column names to fit js convention
    
    * chore: install and setup body-parser middleware
    
    * chore: install http-errors
    
    * refactor: add http error handling
    
    * chore: install nodemon
    
    * refactor: improve logging
    
    * chore: remove unneeded body-parser

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 
 const express = require( "express" );
 const session = require( "express-session" );
+const createError = require( "http-errors" );
+const logger = require('morgan');
 
 const passport = require( "./server/passport-setup" );
 const router = require( "./server/routes" );
@@ -11,6 +13,11 @@ const app = express();
 
 app.set( "views", __dirname + "/dist" );
 app.use( express.static(__dirname + "/dist") );
+
+app.use( logger( "dev", {
+	skip: function (req, res) { return res.statusCode < 400 }
+} ) );
+app.use( express.json() );
 
 app.use( passport.initialize() );
 app.use( passport.session() );
@@ -21,6 +28,25 @@ app.use( session({ secret: "OAuth Session",
 }) );
 
 app.use( "/", router );
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+	next(createError(404));
+});
+  
+// error handler
+app.use(function(err, req, res, next) {
+	res.status(err.status || 500);
+
+	if( !err.expose ) {
+		console.error(`[${new Date().toISOString()}]` + err.message);
+		return res.json({ 
+			message: 'Internal server error.'  
+		});
+	}
+
+	res.json(err)
+});
 
 app.listen( process.env.PORT || 5000, function () {
 	console.log( "Node.js app listening on port 5000!" );
