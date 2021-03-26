4074a328ae7ad2ba69502c5a04d7ec27f4888bfd
Itamar Givon <itamar.givon@wikimedia.de>
2021-03-26 11:14:46 +0100

    chore: add sandboxed queries

--- server/data-access.js
+++ server/data-access.js
@@ -10,12 +10,13 @@ class SensesRepository {
     }
 
     async get( languageCode, languageQID ) {
+        const query = process.env.SANDBOXED 
+            ? queries.sandboxedItemlessSenses()
+            : queries.itemlessSenses( languageCode, languageQID )
         // Error handling is delegated to the caller. 
         // This function will raise all excpetions that axios may raise. 
         // See: https://www.npmjs.com/package/axios#handling-errors
-        const results = await this.queryService.submit(
-            queries.itemlessSenses( languageCode, languageQID )
-        )
+        const results = await this.queryService.submit( query );
 
         return results.map( ( result ) => {
             const senseUri = result.senseId.value;
--- server/sparql-queries.js
+++ server/sparql-queries.js
@@ -25,5 +25,30 @@ module.exports = {
             }
             LIMIT ${numberOfSensesPerRequest}
         `);
+    },
+
+    sandboxedItemlessSenses: () => {
+        const catQid = 'Q3938';
+        const itemForThisSensePid = 'P5137';
+        const translationPid = 'P5972';
+        const numberOfSensesPerRequest = 10;
+        const languageCode = 'en';
+        const languageQid = 'Q3938';
+
+        return sanitize(`
+            SELECT DISTINCT ?lexemeId ?lemma ?senseId ?gloss
+            WHERE {
+            ?lexemeId dct:language wd:${languageQid};
+                        wikibase:lexicalCategory wd:${catQid};
+                        wikibase:lemma ?lemma;
+                        ontolex:sense ?senseId.
+            ?senseId skos:definition ?gloss.
+            FILTER(LANG(?gloss) = "${languageCode}")
+            MINUS { ?senseId wdt:${itemForThisSensePid} [] }
+            MINUS { ?senseId wdt:${translationPid} [] }
+            SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],${languageCode}". }
+            }
+            LIMIT ${numberOfSensesPerRequest}
+        `);
     }
 }
